f92898f3bee7c8799e623e6347d9cf86b5f0420a
Omran <omranelhasadi@gmail.com>
2023-07-22 15:36:16 +0800

    Changed to HashRouter for Github Pages

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# shopping-cart
+# [Live View](https://omranalhasadi.github.io/shopping-cart/)
+
+
+# Description
+
 This is a simple e-commerce cart that I made to test my react skills.
+
+## Prerequisites
+
+You need to have the following installed on your machine before running the project:
+- Node.js
+- npm (comes with Node.js)
+
+## Running the project
+
+- clone the repo
+- cd to the directory where you cloned the repo
+- run "npm install"
+- run "npm start"
--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@
 import './App.css';
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Routes>
         <Route path="/catalog" element={<ProductPage />} />
       </Routes>
     </Router>
